62ff8965a3d18f6bfed37854af5a227585a1890d
Rafael Vieira <rafaellvs322@gmail.com>
2020-08-16 17:05:36 -0300

    FEA: add deployed sequelize connection to models file-generator

--- src/file-generator/models/code-templates.js
+++ src/file-generator/models/code-templates.js
@@ -27,6 +27,7 @@ ${entities.map(entity =>
 
 import { Sequelize, DataTypes } from 'sequelize'
 
+// local connection
 export const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.DATABASE_USER,
@@ -36,6 +37,21 @@ export const sequelize = new Sequelize(
     define: { timestamps: false },
   }
 )
+
+// deployed connection
+// export const sequelize = new Sequelize(process.env.DATABASE_URL,
+//   {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     define: { timestamps: false },
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+// )
 
 const models = {
   ${entities.map(entity =>
