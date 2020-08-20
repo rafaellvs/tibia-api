7a991eafa29652510411f78b7ae90b56a94dde97
Rafael Vieira <rafaellvs322@gmail.com>
2020-08-20 14:31:49 -0300

    ENH: improve formatAttribute utils function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,8 @@
 // converts attributes to snake_case and deals with edge cases
 export const formatAttribute = string => {
   if (string === 'NPC') return 'npc_price'
+  if (string === 'Rune Group') return 'group'
+  if (string === 'Level') return 'lvl'
 
   return string
     ? string.replace(/\n/g, '').replace(/\./g, '').replace(/\s/g, '_').replace(/%/g, '_mod').toLowerCase()
